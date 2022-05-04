1e867310ced023c18e1b399989718f2c859393c5
zhafranth <zhafranthGgmail.com>
2022-05-04 13:02:58 +0700

    fix: modal view

--- src/index.js
+++ src/index.js
@@ -88,13 +88,26 @@ function clickHandler(e) {
   });
 }
 
-// $(".swiper-slide").magnificPopup({
-//   delegate: "a", // child items selector, by clicking on it popup will open
-//   type: "image",
-//   // other options
-// });
-// $(".show-image").magnificPopup({
-//   type: "image",
-//   // other options
-// });
-// $(".show-image").;
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
